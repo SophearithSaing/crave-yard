81cc3bc386c279b6d9ecf45f143eff124bc4b7ad
Sophearith Saing <sophearithsaing123@gmail.com>
2024-11-09 22:49:37 +0700

    Add chart with sample data

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    id: '1',
+    name: 'Saigon Recipe'
+    taste: 9,
+    location: 7,
+    service: 7,
+    price: 3
+    abmiance: 10,
+  },
+  {
+    id: '2',
+    name: 'Khao Kha Moo'
+    taste: 8,
+    location: 6,
+    service: 7,
+    price: 7,
+    ambiance: 5
+  }
+]
--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
 $(() => {
-  console.log('Hello World');
+  const data = {
+    labels: ['Taste', 'Location', 'Service', 'Price', 'Ambiance'],
+    datasets: [
+      {
+        label: 'Saigon Recipe',
+        data: [9, 7, 7, 3, 10],
+      },
+      {
+        label: 'Khao Kha Moo',
+        data: [8, 6, 7, 7, 5],
+      },
+    ],
+  };
+
+  const config = {
+    type: 'radar',
+    data: data,
+    options: {
+      elements: {
+        line: {
+          borderWidth: 2,
+        },
+      },
+      scales: {
+        r: {
+          angleLines: {
+            display: false,
+          },
+          suggestedMin: 5,
+          suggestedMax: 10,
+        },
+      },
+    },
+  };
+
+  new Chart(document.getElementById('chart'), config);
 });
